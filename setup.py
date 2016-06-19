4b0595c973c5d0895a16bcdcb521b2fe5ca6db16
Antonis Christofides <anthony@itia.ntua.gr>
2016-06-19 19:10:15 +0300

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pd2hts",
-    version="DEV",
+    version="0.1.0",
     license="GPL3",
     description=("Read and write Pandas objects from/to Hydrognomon "
                  "timeseries files"),
